2b8182570c447240e1b782587b2ad2779f0b18ef
Lucian-Shaw <Sha24004780@heartofyorkshire.ac.uk>
2025-01-27 16:02:16 +0000

    edgfdegjwec

---
+++ L3Y1-3Dproject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+    public float timer; 
+
+    public float timeLimit;
+
+    public TMP_Text timerText;
+
+    [Header ("Collectables")]
+    public int requiredCollectables;
+
+    public int currentCollectables;
+
+    public TMP_Text collectablesText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = timeLimit;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timerText.text = timer.ToString("F2");
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        if (timer <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        else
+        {
+        timer -= Time.deltaTime;
+        }
+
+        Collectables();
+
+
+        
+    }
+    void Collectables()
+    {
+        collectablesText.text = currentCollectables + "/" + requiredCollectables;
+
+        if (currentCollectables == requiredCollectables)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        
+    }
+}
