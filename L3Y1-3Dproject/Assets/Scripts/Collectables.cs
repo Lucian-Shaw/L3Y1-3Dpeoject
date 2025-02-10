9151be1cd42fc1e3744435358dec5ffd16c0e9c0
Lucian-Shaw <Sha24004780@heartofyorkshire.ac.uk>
2025-02-10 16:00:33 +0000

    CHEESE

---
+++ L3Y1-3Dproject/Assets/Scripts/Collectables.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectables : MonoBehaviour
+{
+   private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GameObject.Find("GameManager").GetComponent<GameManager>().currentCollectables++;
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
